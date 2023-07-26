b990b2cde8a7df084d451c896455d01c5c864af7
Doublewealth <doublewealth92@gmail.com>
2023-07-26 11:41:49 -0700

    Updated

---
+++ string1.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+* _strdup - Duplicates a string
+* @str: The string to duplicate
+*
+* Return: Pointer to duplicated string
+*/
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+*_puts - Print an input string
+*@str: The string to print
+*
+* Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+* _strcpy - Copy a string
+* @dest: The destination
+* @src: The source
+*
+* Return: Pointer to destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+        int i = 0;
+
+        if (dest == src || src == 0)
+                return (dest);
+        while (src[i])
+        {
+                dest[i] = src[i];
+                i++;
+        }
+        dest[i] = 0;
+        return (dest);
+}
+
+/**
+* _putchar - Write the character c to stdout
+* @c: The character to print
+*
+* Return: 1 on success
+* On error, -1 and errno is set appropriately.
+*/
+
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
